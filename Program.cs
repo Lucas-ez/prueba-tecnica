6c874983a0c8b44cdffc1d63c1650780d1d1e39b
lucas-brest <134296037+lucas-brest@users.noreply.github.com>
2024-05-08 17:35:26 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Prueba;
+
+var vagones = new List<int>();
+for(var i = 1; i < 10; i++)
+{
+    vagones.Add(i);
+}
+
+var tren = new Tren(vagones);
+
+
+do
+{
+    tren.MostrarTren();
+
+    Console.WriteLine("\n\n1 - Enganchar vagón");
+    Console.WriteLine("2 - Desenganchar vagón");
+    var option = Console.ReadLine();
+
+
+    if (option == "1")
+    {
+        Console.WriteLine("Ingrese el número del vagón a agregar");
+        var strVagon = Console.ReadLine();
+        var vagon = int.Parse(strVagon);
+
+        var enganche = "";
+        do
+        {
+            Console.WriteLine("1 - Enganchar a la izquierda");
+            Console.WriteLine("2 - Enganchar a la derecha");
+            enganche = Console.ReadLine();
+
+            if (enganche == "1")
+            {
+                tren.EngancharIzquierda(vagon);
+            }
+            else if (enganche == "2")
+            {
+                tren.EngancharDerecha(vagon);
+            }
+
+        } while(enganche != "1" && enganche != "2");
+
+
+    }
+    else if (option == "2")
+    {
+        var enganche = "";
+        do
+        {
+            Console.WriteLine("1 - Desenganchar a la izquierda");
+            Console.WriteLine("2 - Desenganchar a la derecha");
+            enganche = Console.ReadLine();
+            if (enganche == "1")
+            {
+                tren.DesengancharIzquierda();
+            }
+            else if (enganche == "2")
+            {
+                tren.DesengancharDerecha();
+            }
+        } while (enganche != "1" && enganche != "2");
+    }
+} while (true);
---
+++ Tren.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    public class Tren
+    {
+        public List<int> Vagones { get; set; }
+
+        public Tren(List<int> vagones)
+        {
+            Vagones = vagones;
+        }
+
+        public void MostrarTren()
+        {
+            Console.WriteLine("Tren:\n");
+            if (Vagones.Count > 0)
+                Console.Write($"[{Vagones[0]}]");
+
+            for (var i = 1; i < Vagones.Count; i++)
+            {
+                Console.Write($" - [{Vagones[i]}]");
+            }
+        }
+
+        public void EngancharIzquierda(int num)
+        {
+            Vagones.Insert(0, num);
+        }
+
+        public void EngancharDerecha(int num)
+        {
+            Vagones.Add(num);
+        }
+
+        public void DesengancharIzquierda()
+        {
+            Vagones.RemoveAt(0);
+        }
+
+        public void DesengancharDerecha()
+        {
+            Vagones.RemoveAt(Vagones.Count - 1);
+        }
+    }
+}
